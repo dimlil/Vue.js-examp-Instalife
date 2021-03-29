2e1507354b84f5f7fbb029019ab8a96ab7605161
dimlil <d.v.vassilev@gmail.com>
2021-03-29 22:45:58 +0300

    feat/ added home component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
-  <div></div>
+  <Header />
 </template>
 
 <script>
+import Header from "./components/Header.vue"
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header
+  },
 };
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
