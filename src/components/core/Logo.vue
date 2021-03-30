e8bec1ed00a6a9869582626b8c134a06af39bec6
dimlil <d.v.vassilev@gmail.com>
2021-03-30 23:34:26 +0300

    feat/ made logo a link to home

--- src/components/core/Logo.vue
+++ src/components/core/Logo.vue
@@ -1,20 +1,20 @@
 <template>
-  <img src="@/assets/logo.png" class='logo' alt="logo">
+  <router-link to="/">
+    <img src="@/assets/logo.png" class="logo" alt="logo" />
+  </router-link>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
-.logo{
-    width: 150px;
-    height: 50px;
-    object-fit: contain;
+.logo {
+  width: 150px;
+  height: 50px;
+  object-fit: contain;
 }
-.logo:hover{
-    cursor: pointer;
+.logo:hover {
+  cursor: pointer;
 }
 </style>
